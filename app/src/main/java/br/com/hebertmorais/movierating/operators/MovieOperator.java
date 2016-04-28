3a29a7d40d6263bda180be11de6e7774d780f4b5
Hebert Morais <hebertlucashllm@gmail.com>
2016-04-27 23:33:34 -0300

    Some movies url changed

--- app/src/main/java/br/com/hebertmorais/movierating/operators/MovieOperator.java
+++ app/src/main/java/br/com/hebertmorais/movierating/operators/MovieOperator.java
@@ -26,12 +26,12 @@ public class MovieOperator implements MovieOperatorsInterface {
     private List<Movie> moviesCreator() {
 
         ArrayList<Movie> response = new ArrayList<>();
-        Movie movie1 = new Movie("Game of Thrones","Description GOT", 0, "https://pbs.twimg.com/profile_images/702545332475981824/Mg7TpOaw.jpg");
-        Movie movie2 = new Movie("Breaking Bad","Description Breaking Bad", 0, "https://lh6.googleusercontent.com/-70Hlzc775lA/AAAAAAAAAAI/AAAAAAAAAPc/cJmmz52JCGE/s0-c-k-no-ns/photo.jpg");
-        Movie movie3 = new Movie("House MD","Description House", 0, "http://www.bolumrehberi.com/images/tv-show/House-MD/dr_gregory_house_wallpaper_1920x1200_1.jpg");
+        Movie movie1 = new Movie("Game of Thrones","Description GOT", 0, "https://static.omelete.uol.com.br/media/extras/conteudos/gameofthrones_logo_mQkVfzJ_Tpf2KK9.jpg");
+        Movie movie2 = new Movie("Breaking Bad","Description Breaking Bad", 0, "http://2.bp.blogspot.com/-Mfpl-9MdFmk/Vd44vG0OBEI/AAAAAAAAHHk/KFOs_dJLfZg/s620/Breaking-Bad-Season-5.jpg");
+        Movie movie3 = new Movie("House MD","Description House", 0, "http://i.onionstatic.com/avclub/5024/33/16x9/960.jpg");
         Movie movie4 = new Movie("Sons of Anarchy","SOA Description", 0, "http://www.cutthecap.com/wp-content/uploads/2014/10/2684i59B5472A67FEB352.jpg");
-        Movie movie5 = new Movie("Vikings","Vikings Description", 0, "https://pmctvline2.files.wordpress.com/2015/01/vikings_season3_horiz.jpg");
-        Movie movie6 = new Movie("The Walking Dead","TWD Description", 0, "https://images2.alphacoders.com/240/240942.jpg");
+        Movie movie5 = new Movie("Vikings","Vikings Description", 0, "https://static.hbonordic.com/1f10ced-0049c77cbf7/HBON-AAGBK-000-PGM-01-01-2500-HD-169-SR-1920x1080-50-6228414.jpg");
+        Movie movie6 = new Movie("The Walking Dead","TWD Description", 0, "http://www.wallpapertycoon.com/download/4236/2016/02/the_walking_dead_s01e02_games_wallpaper-960x540.jpg/");
         Movie movie7 = new Movie("Friends","Friends Description", 0, "http://static.omelete.uol.com.br/media/extras/conteudos/0aa3afb3cbe3468fc6e43e50070b0810_HQi5OtO.png");
 
         response.add(movie1);
